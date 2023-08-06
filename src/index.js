151ac7fbd237363c9a0eedee7a41b7f5b45a9367
Hadj Said Bouras <hadjsaidbouras@hotmail.com>
2023-08-06 16:14:18 +0100

    Done with the assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    doIt(document.querySelector('#new-task-description').value)
+  })
+})
+
+function doIt(todo) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.textContent = "delete"
+  p.textContent = `${todo} ` 
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+  btn.addEventListener('click', (e) => {
+    e.target.parentNode.remove()
+  })
+}
